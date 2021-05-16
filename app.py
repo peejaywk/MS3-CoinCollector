0654443cc4f4e426588abe0bf7463a3e19f5f369
peejaywk <peejaywk@gmail.com>
2021-05-16 16:33:59 +0000

    Add registration form and associated code to add the user to the database. Add flash message at the top of the page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (Flask, flash, render_template,
                    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,34 @@ mongo = PyMongo(app)
 @app.route("/get_coins")
 def get_coins():
     return render_template("user_coins.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if email address already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"email": request.form.get("email").lower()})
+
+        if existing_user:
+            flash("Email already registered.")
+            return redirect(url_for("register"))
+
+        register = {
+            "fname": request.form.get("fname").lower(),
+            "lname": request.form.get("lname").lower(),
+            "email": request.form.get("email").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("email").lower()
+        flash("Registration successful!")
+        # return redirect(url_for("profile", username=session["user"]))
+        return redirect(url_for("get_coins"))
+
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
